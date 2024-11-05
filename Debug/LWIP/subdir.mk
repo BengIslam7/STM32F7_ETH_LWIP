4dc41e34387c0d1cd462a0a2119c4d52277fc75a
BengIslam7 <devmlpentBMI66@gmail.com>
2024-11-05 18:46:24 +0100

    Adding all files

---
+++ Debug/LWIP/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../LWIP/ethernetif.c 
+
+OBJS += \
+./LWIP/ethernetif.o 
+
+C_DEPS += \
+./LWIP/ethernetif.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+LWIP/%.o LWIP/%.su LWIP/%.cyclo: ../LWIP/%.c LWIP/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F746xx -c -I../Core/Inc -I../LWIP/App -I../LWIP -I../Middlewares/Third_Party/LwIP/src/include -I../Middlewares/Third_Party/LwIP/system -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/LwIP/src/include/netif/ppp -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Middlewares/Third_Party/LwIP/src/include/lwip -I../Middlewares/Third_Party/LwIP/src/include/lwip/apps -I../Middlewares/Third_Party/LwIP/src/include/lwip/priv -I../Middlewares/Third_Party/LwIP/src/include/lwip/prot -I../Middlewares/Third_Party/LwIP/src/include/netif -I../Middlewares/Third_Party/LwIP/src/include/posix -I../Middlewares/Third_Party/LwIP/src/include/posix/sys -I../Middlewares/Third_Party/LwIP/system/arch -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-LWIP
+
+clean-LWIP:
+	-$(RM) ./LWIP/ethernetif.cyclo ./LWIP/ethernetif.d ./LWIP/ethernetif.o ./LWIP/ethernetif.su
+
+.PHONY: clean-LWIP
+
